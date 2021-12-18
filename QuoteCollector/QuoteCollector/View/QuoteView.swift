157349c4222cb03ce464e9b43111bb980c39ee91
Eric Latham <ericoliverlatham@gmail.com>
2021-12-18 18:11:19 -0500

    begin plugging in quote edit/share functionality

--- QuoteCollector/QuoteCollector/View/QuoteView.swift
+++ QuoteCollector/QuoteCollector/View/QuoteView.swift
@@ -8,16 +8,36 @@
 import SwiftUI
 
 struct QuoteView: View {
+
     @ObservedObject var quote: Quote
+
+    @State private var showEditQuoteView = false
+
     var body: some View {
         if quote.exists {
             VStack {
                 Text(quote.text!)
                     .font(.title)
                     .multilineTextAlignment(.center)
-                Text(quote.author)
-                    .font(.body)
-            }.padding()
+                Spacer().frame(maxHeight: 25)
+                Text(quote.author).font(.body)
+            }
+            .padding()
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        showEditQuoteView = true
+                    } label: {
+                        Image(systemName: "pencil.circle")
+                    }
+                }
+            }
+            .sheet(isPresented: $showEditQuoteView) {
+                AddQuoteView(
+                    quoteCollection: quote.collection!,
+                    objectId: quote.objectID
+                )
+            }
         }
         else { EmptyView() }
     }
