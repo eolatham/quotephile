8681cbad6079c5757c4efbf597d571531d490be0
Eric Latham <ericoliverlatham@gmail.com>
2021-12-20 10:46:05 -0500

    finish quote view / copy functionality

--- QuoteCollector/QuoteCollector/View/QuoteView.swift
+++ QuoteCollector/QuoteCollector/View/QuoteView.swift
@@ -2,52 +2,94 @@
 //  QuoteView.swift
 //  QuoteCollector
 //
-//  Created by Eric Latham on 12/12/21.
+//  Created by Eric Latham on 12/19/21.
 //
 
 import SwiftUI
 
 struct QuoteView: View {
+    let copyButtonLabelBefore: Text = Text("Copy").foregroundColor(.accentColor)
+    let copyButtonLabelAfter: Text = Text("Copied").foregroundColor(.green)
 
     @ObservedObject var quote: Quote
 
-    @State private var showEditQuoteView = false
-    @State private var showCopyQuoteView = false
+    @State private var showEditQuoteView: Bool
+    @State private var includeQuotationMarks: Bool
+    @State private var includeAuthor: Bool
+    @State private var authorOnSeparateLine: Bool
+    @State private var copyText: String
+    @State private var copyButtonLabel: Text
+
+    init(quote: Quote) {
+        self.quote = quote
+        self.showEditQuoteView = false
+        self.includeQuotationMarks = true
+        self.includeAuthor = true
+        self.authorOnSeparateLine = false
+        self.copyText = quote.stringify(
+            includeQuotationMarks: true,
+            includeAuthor: true,
+            authorOnSeparateLine: false
+        )
+        self.copyButtonLabel = copyButtonLabelBefore
+    }
+
+    func refresh() {
+        copyText = quote.stringify(
+            includeQuotationMarks: includeQuotationMarks,
+            includeAuthor: includeAuthor,
+            authorOnSeparateLine: authorOnSeparateLine
+        )
+        copyButtonLabel = copyButtonLabelBefore
+    }
 
     var body: some View {
-        if quote.exists {
-            VStack {
-                Text(quote.text!)
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                Spacer().frame(maxHeight: 25)
-                Text(quote.author).font(.body)
-            }
-            .padding()
-            .toolbar {
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
-                    Button {
-                        showEditQuoteView = true
-                    } label: {
-                        Image(systemName: "pencil")
-                    }
-                    Button {
-                        showCopyQuoteView = true
-                    } label: {
-                        Image(systemName: "doc.on.doc")
+        VStack(alignment: .center, spacing: 25) {
+            Text(copyText)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .frame(maxHeight: .infinity)
+                .padding()
+            Form {
+                Section(header: Text("STYLE")) {
+                    Toggle(isOn: $includeQuotationMarks) { Text("Include quotation marks") }
+                        .onChange(of: includeQuotationMarks) { newValue in refresh() }
+                    Toggle(isOn: $includeAuthor) { Text("Include author") }
+                        .onChange(of: includeAuthor) { newValue in refresh() }
+                    if includeAuthor {
+                        Toggle(isOn: $authorOnSeparateLine) { Text("Author on separate line") }
+                            .onChange(of: authorOnSeparateLine) { newValue in refresh() }
                     }
                 }
-            }
-            .sheet(isPresented: $showEditQuoteView) {
-                AddQuoteView(
-                    quoteCollection: quote.collection!,
-                    objectId: quote.objectID
-                )
-            }
-            .sheet(isPresented: $showCopyQuoteView) {
-                CopyQuoteView(quote: quote)
+                Section {
+                    Button(
+                        action: {
+                            let pasteboard = UIPasteboard.general
+                            pasteboard.string = copyText
+                            copyButtonLabel = copyButtonLabelAfter
+                        },
+                        label: {
+                            copyButtonLabel.font(.headline)
+                        }
+                    )
+                    .buttonStyle(PlainButtonStyle())
+                }
             }
         }
-        else { EmptyView() }
+        .toolbar {
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                Button {
+                    showEditQuoteView = true
+                } label: {
+                    Image(systemName: "pencil")
+                }
+            }
+        }
+        .sheet(isPresented: $showEditQuoteView) {
+            AddQuoteView(
+                quoteCollection: quote.collection!,
+                objectId: quote.objectID
+            )
+        }
     }
 }
