810357f644db8baad89c556e60ba9e5f987782b0
Eric Latham <ericoliverlatham@gmail.com>
2021-12-29 17:12:35 -0600

    WIP: replace custom list 'edit parent' functionality with per-item swipe actions for edit and delete

--- QuoteCollector/QuoteCollector/View/List/CustomListView.swift
+++ QuoteCollector/QuoteCollector/View/List/CustomListView.swift
@@ -11,10 +11,11 @@ struct CustomListView<
     EntityPageView: View,
     ConstantListPrefixView: View,
     ConstantListSuffixView: View,
-    AddEntitySheetContentView: View,
-    EditParentSheetContentView: View,
-    BulkEditSheetContentView: View,
-    BulkMoveSheetContentView: View
+    AddEntitySheetView: View,
+    SingleEditSheetView: View,
+    SingleMoveSheetView: View,
+    BulkEditSheetView: View,
+    BulkMoveSheetView: View
 >: View {
     var title: String
     @SectionedFetchRequest var entities: SectionedFetchResults<String, Entity>
@@ -25,24 +26,29 @@ struct CustomListView<
     var entityPageViewBuilder: (Entity) -> EntityPageView
     var constantListPrefixViewBuilder: (() -> ConstantListPrefixView)? = nil
     var constantListSuffixViewBuilder: (() -> ConstantListSuffixView)? = nil
-    var addEntitySheetContentViewBuilder: (() -> AddEntitySheetContentView)? = nil
-    var editParentSheetContentViewBuilder: (() -> EditParentSheetContentView)? = nil
-    var bulkEditSheetContentViewBuilder: (
-        (_ selection: Set<Entity>, _ exitSelectionMode: @escaping () -> Void) -> BulkEditSheetContentView
-        // Use exitSelectionMode if you want to exit selection mode after bulk edit is done
+    var addEntitySheetViewBuilder: (() -> AddEntitySheetView)? = nil
+    var singleEditSheetViewBuilder: ((_ toEdit: Entity) -> SingleEditSheetView)? = nil
+    var singleMoveSheetViewBuilder: ((_ toMove: Entity) -> SingleMoveSheetView)? = nil
+    var singleDeleteFunction: ((_ toDelete: Entity) -> Void)? = nil
+    var singleDeleteAlertMessage: (_ toDelete: Entity) -> String
+        = { _ in return "This action cannot be undone!" }
+    var bulkEditSheetViewBuilder: (
+        // Use exitSelectionMode to exit selection mode after edit is done
+        (_ toEdit: Set<Entity>, _ exitSelectionMode: @escaping () -> Void)
+        -> BulkEditSheetView
     )? = nil
-    var bulkMoveSheetContentViewBuilder: (
-        (_ selection: Set<Entity>, _ exitSelectionMode: @escaping () -> Void) -> BulkMoveSheetContentView
-        // Use exitSelectionMode if you want to exit selection mode after bulk move is done
+    var bulkMoveSheetViewBuilder: (
+        // Use exitSelectionMode to exit selection mode after move is done
+        (_ toMove: Set<Entity>, _ exitSelectionMode: @escaping () -> Void)
+        -> BulkMoveSheetView
     )? = nil
-    var bulkDeleteFunction: ((_ selection: Set<Entity>) -> Void)? = nil
-    var bulkDeleteAlertMessage: (_ selection: Set<Entity>) -> String
+    var bulkDeleteFunction: ((_ toDelete: Set<Entity>) -> Void)? = nil
+    var bulkDeleteAlertMessage: (_ toDelete: Set<Entity>) -> String
         = { _ in return "This action cannot be undone!" }
 
     @State private var selectedEntities: Set<Entity> = []
     @State private var inSelectionMode: Bool = false
     @State private var showAddEntityView: Bool = false
-    @State private var showEditParentView: Bool = false
     @State private var showBulkEditView: Bool = false
     @State private var showBulkMoveView: Bool = false
     @State private var showBulkDeleteAlert: Bool = false
@@ -79,7 +85,11 @@ struct CustomListView<
                     selectedEntities: $selectedEntities,
                     inSelectionMode: inSelectionMode,
                     entityRowViewBuilder: entityRowViewBuilder,
-                    entityPageViewBuilder: entityPageViewBuilder
+                    entityPageViewBuilder: entityPageViewBuilder,
+                    singleEditSheetViewBuilder: singleEditSheetViewBuilder,
+                    singleMoveSheetViewBuilder: singleMoveSheetViewBuilder,
+                    singleDeleteFunction: singleDeleteFunction,
+                    singleDeleteAlertMessage: singleDeleteAlertMessage
                 )
             }
             if !inSelectionMode && constantListSuffixViewBuilder != nil {
@@ -98,11 +108,8 @@ struct CustomListView<
                         selectedSort: $selectedSort,
                         sortOptions: sortOptions
                     )
-                    if addEntitySheetContentViewBuilder != nil {
+                    if addEntitySheetViewBuilder != nil {
                         Button { showAddEntityView = true } label: { Text("Add") }
-                    }
-                    if editParentSheetContentViewBuilder != nil {
-                        Button { showEditParentView = true } label: { Text("Edit") }
                     }
                     Button { enterSelectionMode() } label: { Text("Select") }
                     .disabled(entities.isEmpty)
@@ -112,11 +119,11 @@ struct CustomListView<
                 if inSelectionMode {
                     HStack {
                         let disabled: Bool = selectedEntities.isEmpty
-                        if bulkEditSheetContentViewBuilder != nil {
+                        if bulkEditSheetViewBuilder != nil {
                             Button { showBulkEditView = true }
                                 label: { Text("Edit") }.disabled(disabled)
                         }
-                        if bulkMoveSheetContentViewBuilder != nil {
+                        if bulkMoveSheetViewBuilder != nil {
                             Button { showBulkMoveView = true }
                                 label: { Text("Move") }.disabled(disabled)
                         }
@@ -131,21 +138,17 @@ struct CustomListView<
         .navigationTitle(
             inSelectionMode ? "\(selectedEntities.count) Selected" : title
         )
-        .sheet(isPresented: $showAddEntityView ) {
-            // Only renders when addEntitySheetContentViewBuilder != nil
-            addEntitySheetContentViewBuilder!()
+        .sheet(isPresented: $showAddEntityView) {
+            // Only renders when addEntitySheetViewBuilder != nil
+            addEntitySheetViewBuilder!()
         }
-        .sheet(isPresented: $showEditParentView ) {
-            // Only renders when editParentSheetContentViewBuilder != nil
-            editParentSheetContentViewBuilder!()
+        .sheet(isPresented: $showBulkEditView) {
+            // Only renders when bulkEditSheetViewBuilder != nil
+            bulkEditSheetViewBuilder!(selectedEntities, exitSelectionMode)
         }
-        .sheet(isPresented: $showBulkEditView ) {
-            // Only renders when bulkEditSheetContentViewBuilder != nil
-            bulkEditSheetContentViewBuilder!(selectedEntities, exitSelectionMode)
-        }
-        .sheet(isPresented: $showBulkMoveView ) {
-            // Only renders when bulkMoveSheetContentViewBuilder != nil
-            bulkMoveSheetContentViewBuilder!(selectedEntities, exitSelectionMode)
+        .sheet(isPresented: $showBulkMoveView) {
+            // Only renders when bulkMoveSheetViewBuilder != nil
+            bulkMoveSheetViewBuilder!(selectedEntities, exitSelectionMode)
         }
         .alert(isPresented: $showBulkDeleteAlert) {
             // Only renders when bulkDeleteFunction != nil
@@ -164,28 +167,22 @@ struct CustomListView<
     }
 }
 
-struct CustomListSelectionIcon: View {
-    var isSelected: Bool
-
-    var body: some View {
-        Image(
-            systemName: isSelected
-                ? "checkmark.circle.fill"
-                : "checkmark.circle"
-        )
-    }
-}
-
 struct CustomListSectionView<
     Entity: NSManagedObject,
     EntityRowView: View,
-    EntityPageView: View
+    EntityPageView: View,
+    SingleEditSheetView: View,
+    SingleMoveSheetView: View
 >: View {
     var section: SectionedFetchResults<String, Entity>.Section
     @Binding var selectedEntities: Set<Entity>
     var inSelectionMode: Bool
     var entityRowViewBuilder: (Entity) -> EntityRowView
     var entityPageViewBuilder: (Entity) -> EntityPageView
+    var singleEditSheetViewBuilder: ((Entity) -> SingleEditSheetView)?
+    var singleMoveSheetViewBuilder: ((Entity) -> SingleMoveSheetView)?
+    var singleDeleteFunction: ((Entity) -> Void)?
+    var singleDeleteAlertMessage: (Entity) -> String
 
     private var isSelected: Bool {
         for entity in section {
@@ -222,10 +219,128 @@ struct CustomListSectionView<
                     selectedEntities: $selectedEntities,
                     inSelectionMode: inSelectionMode,
                     rowViewBuilder: entityRowViewBuilder,
-                    pageViewBuilder: entityPageViewBuilder
+                    pageViewBuilder: entityPageViewBuilder,
+                    editSheetViewBuilder: singleEditSheetViewBuilder,
+                    moveSheetViewBuilder: singleMoveSheetViewBuilder,
+                    deleteFunction: singleDeleteFunction,
+                    deleteAlertMessage: singleDeleteAlertMessage
                 )
             }
         }
+    }
+}
+
+struct CustomListItemView<
+    Entity: NSManagedObject,
+    RowView: View,
+    PageView: View,
+    EditSheetView: View,
+    MoveSheetView: View
+>: View {
+    @ObservedObject var entity: Entity
+    @Binding var selectedEntities: Set<Entity>
+    var inSelectionMode: Bool
+    var rowView: RowView
+    var pageView: PageView
+    var editSheetViewBuilder: ((Entity) -> EditSheetView)?
+    var moveSheetViewBuilder: ((Entity) -> MoveSheetView)?
+    var deleteFunction: ((Entity) -> Void)?
+    var deleteAlertMessage: (Entity) -> String
+
+    private var isSelected: Bool
+
+    @State private var showEditView: Bool = false
+    @State private var showMoveView: Bool = false
+    @State private var showDeleteAlert: Bool = false
+
+    init(
+        entity: Entity,
+        selectedEntities: Binding<Set<Entity>>,
+        inSelectionMode: Bool,
+        @ViewBuilder rowViewBuilder: (Entity) -> RowView,
+        @ViewBuilder pageViewBuilder: (Entity) -> PageView,
+        editSheetViewBuilder: ((Entity) -> EditSheetView)?,
+        moveSheetViewBuilder: ((Entity) -> MoveSheetView)?,
+        deleteFunction: ((Entity) -> Void)?,
+        deleteAlertMessage: @escaping (Entity) -> String
+    ) {
+        self.entity = entity
+        _selectedEntities = selectedEntities
+        self.inSelectionMode = inSelectionMode
+        self.rowView = rowViewBuilder(entity)
+        self.pageView = pageViewBuilder(entity)
+        self.editSheetViewBuilder = editSheetViewBuilder
+        self.moveSheetViewBuilder = moveSheetViewBuilder
+        self.deleteFunction = deleteFunction
+        self.deleteAlertMessage = deleteAlertMessage
+        self.isSelected = selectedEntities.wrappedValue.contains(entity)
+    }
+
+    var body: some View {
+        VStack {
+            if inSelectionMode {
+                HStack {
+                    Button {
+                        if isSelected { selectedEntities.remove(entity) }
+                        else { selectedEntities.update(with: entity) }
+                    } label: { CustomListSelectionIcon(isSelected: isSelected) }
+                    rowView
+                }.foregroundColor(isSelected ? .accentColor : .primary)
+            } else {
+                NavigationLink { pageView } label: { rowView }
+                .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                    if editSheetViewBuilder != nil {
+                        Button { showEditView = true } label: { Text("Edit") }
+                        .tint(.accentColor)
+                    }
+                    if moveSheetViewBuilder != nil {
+                        Button { showMoveView = true } label: { Text("Move") }
+                        .tint(.orange)
+                    }
+                    if deleteFunction != nil {
+                        Button { showDeleteAlert = true } label: { Text("Delete") }
+                        .tint(.red)
+                    }
+                }
+            }
+        }
+        .sheet(isPresented: $showEditView) {
+            // Only renders when editSheetViewBuilder != nil
+            editSheetViewBuilder!(entity)
+        }
+        .sheet(isPresented: $showMoveView) {
+            // Only renders when moveSheetViewBuilder != nil
+            moveSheetViewBuilder!(entity)
+        }
+        .alert(isPresented: $showDeleteAlert) {
+            // Only renders when deleteFunction != nil
+            Alert(
+                title: Text("Are you sure?"),
+                message: Text(deleteAlertMessage(entity)),
+                primaryButton: .destructive(Text("Yes, delete")) {
+                    withAnimation {
+                        deleteFunction!(entity)
+                    }
+                },
+                secondaryButton: .cancel(Text("No, cancel"))
+            )
+        }
+    }
+}
+
+struct CustomListSortSelectView<E>: View {
+    @Binding var selectedSort: Sort<E>
+    let sortOptions: [Sort<E>]
+
+    var body: some View {
+        Menu {
+            Picker("Sort By", selection: $selectedSort) {
+                ForEach(sortOptions, id: \.self) { sort in
+                    Text(sort.name)
+                }
+            }
+        } label: { Text("Sort") }
+        .pickerStyle(.inline)
     }
 }
 
@@ -250,60 +365,14 @@ struct CustomListSectionHeaderView: View {
     }
 }
 
-struct CustomListItemView<
-    Entity: NSManagedObject,
-    RowView: View,
-    PageView: View
->: View {
-    @ObservedObject var entity: Entity
-    @Binding var selectedEntities: Set<Entity>
-    var inSelectionMode: Bool
-
-    let rowView: RowView
-    let pageView: PageView
-
-    private let isSelected: Bool
-
-    init(
-        entity: Entity,
-        selectedEntities: Binding<Set<Entity>>,
-        inSelectionMode: Bool,
-        @ViewBuilder rowViewBuilder: (Entity) -> RowView,
-        @ViewBuilder pageViewBuilder: (Entity) -> PageView
-    ) {
-        self.entity = entity
-        _selectedEntities = selectedEntities
-        self.inSelectionMode = inSelectionMode
-        self.rowView = rowViewBuilder(entity)
-        self.pageView = pageViewBuilder(entity)
-        self.isSelected = selectedEntities.wrappedValue.contains(entity)
-    }
+struct CustomListSelectionIcon: View {
+    var isSelected: Bool
 
     var body: some View {
-        if inSelectionMode {
-            HStack {
-                Button {
-                    if isSelected { selectedEntities.remove(entity) }
-                    else { selectedEntities.update(with: entity) }
-                } label: { CustomListSelectionIcon(isSelected: isSelected) }
-                rowView
-            }.foregroundColor(isSelected ? .accentColor : .primary)
-        } else { NavigationLink { pageView } label: { rowView } }
-    }
-}
-
-struct CustomListSortSelectView<E>: View {
-    @Binding var selectedSort: Sort<E>
-    let sortOptions: [Sort<E>]
-
-    var body: some View {
-        Menu {
-            Picker("Sort By", selection: $selectedSort) {
-                ForEach(sortOptions, id: \.self) { sort in
-                    Text(sort.name)
-                }
-            }
-        } label: { Text("Sort") }
-        .pickerStyle(.inline)
+        Image(
+            systemName: isSelected
+                ? "checkmark.circle.fill"
+                : "checkmark.circle"
+        )
     }
 }
